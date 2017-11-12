d164094beee8f30654c3a23dda2203695ec0156e
LakshaJael <nihongotraveler@icloud.com>
2017-11-12 13:26:19 +0800

    Made a bubble_sort program

---
+++ MediumExercises/bubble_sort.rb
@@ -0,0 +1,23 @@
+def bubble_sort!(array)
+  y = array.length-1
+  i = 1
+  until y == 0
+    swap = false
+    while i <= y
+      a = array[i]
+      b = array[i-1]
+      if array[i-1] > array[i]
+        array[i-1] = a
+        array[i] = b
+        swap = true
+      end
+      i += 1
+    end
+    p swap
+    return array if swap == false
+    y -= 1
+    i = 1
+  end
+end
+
+p bubble_sort!([6,2,7,1,4,9])
